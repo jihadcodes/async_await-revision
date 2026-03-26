57d60927b6f9db93a75b234f7d030922a92e8427
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-26 12:36:03 +0600

    todo list design started

---
+++ Async  Await/script.js
@@ -0,0 +1,20 @@
+// console.log("Hello");
+
+
+// async function fetchData() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     console.log( await response.json());
+// }
+// fetchData();
+
+// console.log("Hi There");
+// console.log("Some other code");
+
+
+
+
+
+
+
+
+
