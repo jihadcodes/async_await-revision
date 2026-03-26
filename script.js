29c9f54e1aa6da9b126f327544c279b9eb3da9c0
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-26 11:52:36 +0600

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+
+
+const result = fetch('https://jsonplaceholder.typicode.com/users')
+
+console.log(result);
+console.log("Hi There");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+
+
+
+
+
+
+
