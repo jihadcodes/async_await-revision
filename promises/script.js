453830973b55acd758db02132e71a1995730b5e4
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-03 21:44:16 +0600

    promises examle

--- promises/script.js
+++ promises/script.js
@@ -1,16 +1,15 @@
-
-// Promise = Promise is a JavaScript object that represents the eventual completion or failure of an asynchronous operation. 
+// Promise = Promise is a JavaScript object that represents the eventual completion or failure of an asynchronous operation.
 
 const promise = new Promise((resolve, reject) => {
-  let success = false;
+  let val = Math.floor(Math.random() * 10) + 1; // 1 থেকে 10 পর্যন্ত একটি র্যান্ডম সংখ্যা
 
-  if (success) {
-    resolve("খাবার আসছে!"); // ✅
+  if (val > 5) {
+    resolve("   resolved with success "  +  val); // ✅
   } else {
-    reject("খাবার আসবে না!"); // ❌
+    reject("  rejected with error "  +  val); // ❌
   }
 });
 
 promise
-  .then((result) => console.log(result))  // success হলে
-  .catch((error) => console.log(error));  // error হলে
+  .then((result) => console.log(result)) // success হলে
+  .catch((error) => console.log(error)); // error হলে
